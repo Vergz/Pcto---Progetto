c3d064142c685da76a52f383295337fd1c3575eb
Bergamo <riccardobergamo2016@gmail.com>
2022-06-01 09:04:00 +0200

    bootstrap + js fix

--- Home/home.php
+++ Home/home.php
@@ -587,5 +587,31 @@
 			</form>
 		</div>
 
+<script>
+	const myForm = document.getElementById('myForm');
+	const inpFile = document.getElementById('inpFile');
+	console.log(myForm);
+	console.log(inpFile);
+
+	myForm.addEventListener("submit", e => {
+	e.preventDefault();
+
+	const endpoint = "upload.php";
+	const formData = new FormData();
+
+	formData.append("inpFile", inpFile.files[0]);
+
+	fetch(endpoint , {
+		method: "post",
+		body: formData
+	}).catch(console.error);
+	});
+
+			function ricarica(){
+				setTimeout(() => {
+					window.location.reload();
+				}, 250);
+			}
+</script>
 </body>
 </html>
